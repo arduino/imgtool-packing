6c66268a9820ca66bef488dd4b0389a70357eaf9
umbynos <umberto.baldi@edu.unito.it>
2022-02-11 17:28:38 +0100

    test for pyinstaller guys

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ python -m pip install --upgrade pip setuptools wheel
 cd workspace/imgtool
 pip install -r ../requirements.txt
 pip install pyinstaller==4.2
+ls /usr/local/lib/python3.7/site-packages/PyInstaller/bootloader/
+uname -m
 pyinstaller --onefile main.py -n imgtool
 file dist/imgtool
 ldd dist/imgtool
