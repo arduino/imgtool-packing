a3747c728d8859c71a5d1d95769d09b10770b0ee
umbynos <umberto.baldi@edu.unito.it>
2022-02-14 16:35:55 +0100

    fix imgtool not starting. imgtool has to be run from `scripts/` folder and not from `imgtool/` one. Otherwise it will pick up the wrong main.py

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 apt update
 apt install -y upx
 python -m pip install --upgrade pip setuptools wheel
-cd workspace/imgtool
-pip install -r ../requirements.txt
+cd workspace/
+pip install -r requirements.txt
 pip install pyinstaller==4.2
 ls /usr/local/lib/python3.7/site-packages/PyInstaller/bootloader/
 uname -m
