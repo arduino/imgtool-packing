40ba5500f0481a4cc9b7a6e56f83bf2f9586b973
Umberto Baldi <u.baldi@arduino.cc>
2022-02-17 15:57:38 +0100

    better organize the print output
    Co-authored-by: per1234 <accounts@perglass.com>

--- build.sh
+++ build.sh
@@ -6,4 +6,22 @@ python -m pip install --upgrade pip setuptools wheel
 cd workspace/
 pip install -r requirements.txt
 pip install pyinstaller==4.2
+echo "
+---
+Available PyInstaller bootloaders:"
+ls /usr/local/lib/python3.7/site-packages/PyInstaller/bootloader/
+echo "
+---
+Kernel architecture name:"
+uname -m
+echo "
+---"
 pyinstaller --onefile main.py -n imgtool
+echo "
+---
+File information for build output:"
+file dist/imgtool
+echo "
+---
+Library dependencies for build output:"
+ldd dist/imgtool
