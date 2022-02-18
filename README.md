bfffe92c8df7daae2dca29a66d1abf92bae090c2
Umberto Baldi <34278123+umbynos@users.noreply.github.com>
2022-02-18 18:36:07 +0100

    Add release workflow and complementary files  (#1)
    * Update README.md
    
    * add license, the same used in imgtool repo
    
    * add patches, apply them on top of 1.8.0
    
    0009 is used to patch cryptography version used because of: https://github.com/healthchecks/healthchecks/issues/565
    
    * [WIP] add first draft of release wf
    
    [TODO] remove hardcoded version and use ${GITHUB_REF/refs\/tags\//}
    
    add bash as default shell to find zip on win
    
    use 7zip on win to archive, zip is not installed by default
    
    remove ${{ github.workspace }} from win, it does not get expanded correctly
    
    * fix mac archive being corrupted
    
    tar in macos is not working correctly on the ghactions hosted runners https://github.com/actions/virtual-environments/issues/2619
    
    * add build using qemu and crosscompile with docker containers
    
    * fix path of volume binding, without the absolute path the volume is empty
    
    * fix permission problem: dist dir is created in the container with different user/grp
    
    * try to fix armv6 and v7
    
    * install all qemu platforms, the build time does not increase
    
    * use version 4.2 of pyinstaller [it has the bootloader 🎉 for Linux-32-arm] https://github.com/pyinstaller/pyinstaller/issues/6532#issuecomment-1035215035
    
    * use arm32v5 instead of arm32v6 as target arch. Debian is not available for armv6.
    arm32v6 arch should be able to run arm32v5 binaries
    
    * test for pyinstaller guys
    
    * bring back runner version because of glibc too recent
    
    * try to run file produced by pyinstaller
    
    * fix imgtool not starting. imgtool has to be run from `scripts/` folder and not from `imgtool/` one. Otherwise it will pick up the wrong main.py
    
    * use `env.PROJECT_NAME`
    
    * finalize CI: add correct trigger, create-release step, step names & cleanup
    
    * Apply suggestions from code review
    
    Co-authored-by: per1234 <accounts@perglass.com>
    
    * better organize the print output
    
    Co-authored-by: per1234 <accounts@perglass.com>
    
    * use env vars to factor out path strings
    
    Co-authored-by: per1234 <accounts@perglass.com>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # imgtool-packing
+
+This repo does not contain the source code, but only the patches, and the release workflow
+
+## Sources
+The sources can be found in the [official repo](https://github.com/mcu-tools/mcuboot/tree/main/scripts)
+
+## Imgtool-docs
+https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/mcuboot/imgtool.html
---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y upx
+python -m pip install --upgrade pip setuptools wheel
+cd workspace/
+pip install -r requirements.txt
+pip install pyinstaller==4.2
+echo "
+---
+Available PyInstaller bootloaders:"
+ls /usr/local/lib/python3.7/site-packages/PyInstaller/bootloader/
+echo "
+---
+Kernel architecture name:"
+uname -m
+echo "
+---"
+pyinstaller --onefile main.py -n imgtool
+echo "
+---
+File information for build output:"
+file dist/imgtool
+echo "
+---
+Library dependencies for build output:"
+ldd dist/imgtool
